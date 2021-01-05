3632180158eb7365d667cf32a07e9407e617c9fe
Ben Davenport <46979432+ben-davenport@users.noreply.github.com>
2021-01-05 13:40:06 -0800

    Create assignment2.py

---
+++ assignment2.py
@@ -0,0 +1,12 @@
+# This file contains all of the 100,000 integers between 1 and 100,000 (inclusive)
+# in some order, with no integer repeated.
+
+# Your task is to compute the number of inversions in the file given, where the ithi^{th}ith row
+# of the file indicates the ithi^{th}ith entry of an array.
+
+# Because of the large size of this array, you should implement the 
+# fast divide-and-conquer algorithm covered in the video lectures.
+
+# [TIP: before submitting, first test the correctness of your program on some small test files or your own devising.]
+
+# import integers from ./integerArray.txt
