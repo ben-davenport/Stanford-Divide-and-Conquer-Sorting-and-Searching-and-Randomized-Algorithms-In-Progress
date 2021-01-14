95006aa1fa98a0fb44192f5538ab1349b38d68af
Ben Davenport <46979432+ben-davenport@users.noreply.github.com>
2021-01-14 14:41:34 -0800

    Update assignment2.py
    updated assignment 2

--- assignment2.py
+++ assignment2.py
@@ -10,3 +10,44 @@
 # [TIP: before submitting, first test the correctness of your program on some small test files or your own devising.]
 
 # import integers from ./integerArray.txt
+
+
+with open("./integerArray.txt") as file:
+    lines =[line.rstrip() for line in file]
+testArray = [123, 456, 789, 345, 678, 910, 1111, 2211, 212]
+counter =0
+def countInversions(array1):
+    global counter
+    if(len(array1) > 1):
+        a = array1[:(len(array1) // 2)]
+        b = array1[(len(array1) // 2):]
+        countInversions(a)
+        countInversions(b)
+        countA = 0
+        countB = 0
+        countC = 0
+        while countA < len(a) and countB < len(b):
+            if a[countA] < b[countB]:
+                array1[countC] = a[countA]
+                countA = countA+1
+            else: 
+                array1[countC] = b[countB]
+                countB = countB+1
+                counter = counter+(len(a)-countA)
+            countC = countC+1
+            
+        while countA < len(a):
+            array1[countC]=a[countA]
+            countA = countA +1
+            countC = countC+1
+            
+        while countB < len(b):
+            array1[countC]=b[countB]
+            countB = countB +1
+            countC = countC+1
+            
+#         print('counter: {}'.format(counter))
+    return counter
+# testArray = [8, 4, 2, 1]
+# countInversions(testArray)
+countInversions(lines)
